5814ed9c9b09f69aedb0deb5713f57162adf3f5d
Gustaf Ekström <gusek93@gmail.com>
2023-12-09 18:35:38 +0100

    sorted sensordata from each sensor and displayed on page.

--- home-app-frontend/src/components/TemperaturePlot.tsx
+++ home-app-frontend/src/components/TemperaturePlot.tsx
@@ -12,16 +12,44 @@ interface TemperaturePlotProps {
 }
 
 const TemperaturePlot = ({ temperatureData }: TemperaturePlotProps) => {
+
+  //Split temperature readings into 5 arrays containing info from each sensor. 
+  // Data is stored in sensordata array. Each index represents a sensor and its values.
+  const sensordata: Number[][] = [];
+  for(let i = 0; i < 5; i++){
+    const sensorReadings: Number[] = [];
+    for(let j = 0; j < temperatureData.length; j++){
+      const temperatureReading = temperatureData[j].temperature_readings[i];
+      sensorReadings.push(temperatureReading);
+    }
+    sensordata.push(sensorReadings);
+  }
+  
+  
+  console.log(sensordata);
+  /*
+  {temperatureData.map((element, index) => (
+    <li key={index}>
+      <h3>Sensor {index}</h3>
+      Date: {element.date}, Temperatures:{" "}
+      {element.temperature_readings.join(", ")}
+    </li>
+  ))}
+  */
+
   return (
     <div>
-      <h1>Temperature Data</h1>
+      <h1>Sensordata</h1>
       <ul>
-        {temperatureData.map((element, index) => (
+
+        {sensordata.map((element, index) => (
           <li key={index}>
-            Date: {element.date}, Temperatures:{" "}
-            {element.temperature_readings.join(", ")}
+            <h3>Sensor {index+1}</h3>
+            Temperatures:{" "}
+            {element.join(", ")}
           </li>
         ))}
+        
       </ul>
     </div>
   );
