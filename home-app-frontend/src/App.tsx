f1b0b387339992f3fa4e9fc5229323b846469af7
Gustaf Ekström <gusek93@gmail.com>
2024-01-27 21:27:53 +0100

    övergång till Raspberry

--- home-app-frontend/src/App.tsx
+++ home-app-frontend/src/App.tsx
@@ -1,13 +1,7 @@
-//import axios from "axios";
+import axios from "axios";
 import { useEffect, useState } from "react";
-<<<<<<< Updated upstream
 import TemperaturePlot from "./components/TemperaturePlot/TemperaturePlot";
 import "./App.css";
-=======
-import TemperaturePlot from "./components/TemperaturePlot";
-import StatPlot from "./components/StatPlot";
-import "./App.css"
->>>>>>> Stashed changes
 
 interface TemperatureData {
   Time: String;
@@ -15,18 +9,19 @@ interface TemperatureData {
 }
 
 interface StatData {
-  maxRadOut: String, 
-  minOutdoorTemp: String, 
-  maxOutdoorTemp: String, 
-  maxVpOut: String
+  maxRadOut: String;
+  minOutdoorTemp: String;
+  maxOutdoorTemp: String;
+  maxVpOut: String;
 }
-
 
 const App = () => {
   const [temperatureData, setTemperatureData] = useState<TemperatureData[]>([]);
   const [statData, setStatData] = useState<StatData[]>([]);
 
   // Code for generating test Data used for development.
+
+  /*
   useEffect(() => {
     generateTestData();
   }, []);
@@ -56,9 +51,9 @@ const App = () => {
     }
     setTemperatureData(fixData(testData));
   };
-
+*/
   // Code used to fetch data from Backend. Uncomment when Ready
-  /*
+
   useEffect(() => {
     const fetchTemperatureData = async () => {
       try {
@@ -71,27 +66,30 @@ const App = () => {
       } catch (error) {
         console.error("Error fetching temperature values : ", error);
       }
+    };
 
+    const fetchStatData = async () => {
       try {
         const response = await axios.get(
           "http://localhost:5000/min_max_temperature_data"
         );
         const data = response.data;
-        const modifiedData: StatData[] = fixStatData(data);
-        setStatData(modifiedData);
+        console.log(data);
+        //const modifiedData: StatData[] = fixStatData(data);
+        //setStatData(modifiedData);
       } catch (error) {
         console.error("Error fetching min max values : ", error);
       }
-
     };
 
     fetchTemperatureData();
+    fetchStatData();
 
-    setInterval(()=>{
+    setInterval(() => {
       fetchTemperatureData();
-    }, 5000)
+      fetchStatData();
+    }, 5000);
   }, []);
-  */
 
   const fixData = (rawData: Array<String[]>): TemperatureData[] => {
     const fixedData: TemperatureData[] = rawData.map((item: String[]) => {
@@ -102,19 +100,12 @@ const App = () => {
     return fixedData;
   };
 
-<<<<<<< Updated upstream
-=======
-  const fixStatData = (rawData: Array<String[]>): StatData[] => {
-    const fixedData: StatData[] = rawData.map((item: String[]) => {
-      return {maxRadOut: item[0], minOutdoorTemp: item[1], maxOutdoorTemp: item[2], maxVpOut: item[3]}
-    });
-    return fixedData;
-  }
-
-
-  
   /*
->>>>>>> Stashed changes
+  const fixStatData = (data: string[]): StatData => {
+    
+  };
+  */
+
   return (
     <div className="app">
       <h2 className="app-headline">Temperature Measurement App</h2>
@@ -122,19 +113,7 @@ const App = () => {
         <TemperaturePlot data={temperatureData} />
       </div>
     </div>
-<<<<<<< Updated upstream
   );
-=======
-    
-    <h2>Statistics</h2>
-    <div>
-//      <StatPlot data={statData} />
-    </div>
-   
-    
-  </div>
- );
->>>>>>> Stashed changes
 };
 
 export default App;
